9504d28757d0c5776fd62e8717c8c78f56a92a11
long2ice <long2ice@gmail.com>
2022-11-20 23:21:41 +0800

    feat: improve add page

--- src/modules/pages/add/add.tsx
+++ src/modules/pages/add/add.tsx
@@ -1,5 +1,14 @@
 import { View } from "@tarojs/components";
-import { Button, Cell, Field, Input, Form, Toast } from "@taroify/core";
+import {
+  Button,
+  Cell,
+  Field,
+  Input,
+  Form,
+  Toast,
+  Popup,
+  Picker,
+} from "@taroify/core";
 import { useState } from "react";
 import { TOTP, URI } from "otpauth";
 import { navigateBack } from "@tarojs/taro";
@@ -14,8 +23,12 @@ export default function Add() {
   const [account, setAccount] = useState("");
   const [secret, setSecret] = useState("");
   const [issuer, setIssuer] = useState("");
+  const [digits, setDigits] = useState(6);
+  const [period, setPeriod] = useState(30);
+  const [algorithm, setAlgorithm] = useState("SHA1");
+  const [openPicker, setOpenPicker] = useState(false);
   const submit = async () => {
-    const uri = `otpauth://totp/${issuer}:${account}?secret=${secret}&issuer=${issuer}`;
+    const uri = `otpauth://totp/${issuer}:${account}?secret=${secret}&issuer=${issuer}&digits=${digits}&period=${period}&algorithm=${algorithm}`;
     const totp = URI.parse(uri) as TOTP;
     await addOTP(totp);
     await api.addOTPs([totp.toString()]);
@@ -37,6 +50,7 @@ export default function Add() {
             <Form.Control>
               <Input
                 value={account}
+                placeholder="请输入账户"
                 onChange={(e) => setAccount(e.detail.value)}
               />
             </Form.Control>
@@ -49,24 +63,93 @@ export default function Add() {
             <Form.Control>
               <Input
                 password
+                placeholder="请输入密钥"
                 value={secret}
                 onChange={(e) => setSecret(e.detail.value)}
               />
             </Form.Control>
           </Form.Item>
           <Field name="issuer" label={{ align: "left", children: "颁发者" }}>
-            <Input value={issuer} onChange={(e) => setIssuer(e.detail.value)} />
+            <Input
+              placeholder="请输入颁发者"
+              value={issuer}
+              onChange={(e) => setIssuer(e.detail.value)}
+            />
           </Field>
+          <Field name="digits" label={{ align: "left", children: "长度" }}>
+            <Input
+              type="number"
+              value={digits.toString()}
+              onChange={(e) => setDigits(parseInt(e.detail.value))}
+            />
+          </Field>
+          <Field name="period" label={{ align: "left", children: "周期" }}>
+            <Input
+              type="number"
+              value={period.toString()}
+              onChange={(e) => setPeriod(parseInt(e.detail.value))}
+            />
+          </Field>
+          <Field
+            name="algorithm"
+            label={{ align: "left", children: "算法" }}
+            onClick={() => setOpenPicker(true)}
+          >
+            <Input
+              value={algorithm}
+              readonly
+              onChange={(e) => setAlgorithm(e.detail.value)}
+            />
+          </Field>
+          <Popup
+            open={openPicker}
+            rounded
+            placement="bottom"
+            onClose={setOpenPicker}
+          >
+            <Popup.Backdrop />
+            <Picker
+              onCancel={() => setOpenPicker(false)}
+              onConfirm={(v) => {
+                setOpenPicker(false);
+                setAlgorithm(v[0]);
+              }}
+            >
+              <Picker.Toolbar>
+                <Picker.Button>取消</Picker.Button>
+                <Picker.Title>算法</Picker.Title>
+                <Picker.Button>确认</Picker.Button>
+              </Picker.Toolbar>
+              <Picker.Column>
+                <Picker.Option>SHA1</Picker.Option>
+                <Picker.Option>SHA224</Picker.Option>
+                <Picker.Option>SHA256</Picker.Option>
+                <Picker.Option>SHA384</Picker.Option>
+                <Picker.Option>SHA512</Picker.Option>
+                <Picker.Option>SHA3-224</Picker.Option>
+                <Picker.Option>SHA3-256</Picker.Option>
+                <Picker.Option>SHA3-384</Picker.Option>
+                <Picker.Option>SHA3-512</Picker.Option>
+              </Picker.Column>
+            </Picker>
+          </Popup>
         </Cell.Group>
         <View style={{ margin: "16px" }}>
+          <Button onClick={submit} shape="round" block color="primary">
+            提交
+          </Button>
           <Button
-            onClick={submit}
             shape="round"
             block
-            color="primary"
-            formType="submit"
+            color="warning"
+            style={{ marginTop: "16px" }}
+            onClick={() => {
+              setAccount("");
+              setSecret("");
+              setIssuer("");
+            }}
           >
-            提交
+            重置
           </Button>
         </View>
       </Form>
